a826abe0056085505aa270824109f0acede76e94
Paulo Gomes <paulo.rgomes22@gmail.com>
2021-09-29 16:52:32 -0300

    docs(storybook): configurando addons
    Adicionando addons no projeto: Notes e Knobs
    
    BREAKING CHANGE: To fix duplicate imports, all Sass placeholders have moved from the
    utilities/mixins file to its own utilities/extends file.

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,9 +2,13 @@ import React from "react";
 import { configure, addDecorator } from "@storybook/react";
 import { ThemeProvider } from "@shopify/restyle";
 import theme from "../themes/default";
+import { withKnobs } from '@storybook/addon-knobs';
+import { withNotes } from '@storybook/addon-notes';
 
 // Wrap all stories in Theme Provider
 addDecorator((story) => <ThemeProvider theme={theme}>{story()}</ThemeProvider>);
+addDecorator(withKnobs);
+addDecorator(withNotes);
 
 // automatically import all files ending in *.stories.js
 configure(require.context("../components", true, /\.stories\.[tj]sx$/), module);
