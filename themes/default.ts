e2ba00147c8df6f11721c5081b8690ba99d23c81
Paulo Gomes <paulo.rgomes22@gmail.com>
2021-09-30 11:54:26 -0300

    chore(theme): ajustando warnings do lint
    fazendo ajustes de warnings de virgula nos ultimos itens de objetos

--- themes/default.ts
+++ themes/default.ts
@@ -10,25 +10,25 @@ const palette = {
   greenDark: '#0A906E',
 
   black: '#0B0B0B',
-  white: '#F0F2F3'
+  white: '#F0F2F3',
 };
 
 const theme = createTheme({
   colors: {
     primary: palette.purplePrimary,
     secondary: palette.greenPrimary,
-    ...palette
+    ...palette,
   },
   spacing: {
     s: 8,
     m: 16,
     l: 24,
-    xl: 40
+    xl: 40,
   },
   breakpoints: {
     phone: 0,
-    tablet: 768
-  }
+    tablet: 768,
+  },
 });
 
 export type Theme = typeof theme;
