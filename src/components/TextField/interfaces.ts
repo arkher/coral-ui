cca4a208d561c355ed10ee4e44b87d1941a86c60
Micael Gomes <micaellgoms@gmail.com>
2021-11-17 18:52:50 -0300

    feat(textarea): contador de caracteres para o textarea

--- src/components/TextField/interfaces.ts
+++ src/components/TextField/interfaces.ts
@@ -1,3 +1,5 @@
+import { KeyboardTypeOptions } from 'react-native';
+
 type TextFieldProps = {
   variant: 'small' | 'medium';
 } & Partial<{
@@ -5,6 +7,8 @@ type TextFieldProps = {
   assistiveText: string;
   placeholder: string;
   status: 'success' | 'error' | undefined;
+  keyboardType: KeyboardTypeOptions;
+  autoCapitalize: 'none' | 'sentences' | 'words' | 'characters' | undefined;
 }>;
 
 export { TextFieldProps };
