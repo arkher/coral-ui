080db6eae91c0ef88830dcb14b998d4e0ecbe053
Micael Machado Gomes <micael.gomes@grupomateus.com.br>
2021-11-18 20:39:16 +0000

    Feedback e ref para os inputs

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { SafeAreaView } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { useTheme } from '@shopify/restyle';
@@ -8,6 +8,7 @@ import Text from '../Text';
 import Box from '../Box';
 import Input from '../Input';
 import { TextFieldProps } from './interfaces';
+import { InputFowardEvents } from '../Input/interfaces';
 
 const TextField: React.FC<TextFieldProps> = ({
   label,
@@ -15,8 +16,28 @@ const TextField: React.FC<TextFieldProps> = ({
   variant,
   assistiveText,
   status,
+  keyboardType,
+  autoCapitalize,
+  returnKeyType,
 }) => {
   const { colors } = useTheme<Theme>();
+  const textfieldRef = useRef<InputFowardEvents>(null);
+
+  useEffect(() => {
+    if (status === 'error') {
+      textfieldRef.current?.error();
+    }
+
+    if (status === 'success') {
+      textfieldRef.current?.success();
+    }
+  }, [status]);
+
+  const statusKeyPair = {
+    error: colors.feedbackErrorBase,
+    success: colors.feedbackSuccessBase,
+    default: colors.neutralDark,
+  };
 
   return (
     <SafeAreaView>
@@ -26,25 +47,36 @@ const TextField: React.FC<TextFieldProps> = ({
         </Text>
       )}
       <Input
+        ref={textfieldRef}
         placeholder={placeholder}
-        placeholderTextColor={colors.neutralDark}
         variant={variant}
+        keyboardType={keyboardType}
+        autoCapitalize={autoCapitalize}
+        returnKeyType={returnKeyType}
         my="quarck"
         p="nano"
         icon="close"
+        style={{
+          flex: 1,
+        }}
       />
       {!!assistiveText && (
-        <Box flexDirection="row" alignItems="baseline">
-          <Icon
-            name={
-              status === 'success'
-                ? 'check-circle-outline'
-                : 'information-outline'
-            }
-            color={colors.neutralDark}
-            size={16}
-          />
-          <Text mx="quarck" color="neutralDark">
+        <Box flexDirection="row" alignItems="center">
+          {status === 'success' && (
+            <Icon
+              name="check-circle-outline"
+              size={24}
+              color={statusKeyPair[status]}
+            />
+          )}
+          {status === 'error' && (
+            <Icon
+              name="alert-circle-outline"
+              size={24}
+              color={statusKeyPair[status]}
+            />
+          )}
+          <Text ml="quarck" fs="xxxxs" color="neutralDarkest">
             {assistiveText}
           </Text>
         </Box>
