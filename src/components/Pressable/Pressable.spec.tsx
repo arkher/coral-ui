077dd26d38303c3fe2baed55ba24e45854be6608
Paulo Gomes <paulo.rgomes22@gmail.com>
2021-12-07 19:10:33 -0300

    feat(pressable): criando componente

---
+++ src/components/Pressable/Pressable.spec.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { cleanup, fireEvent, render } from '@testing-library/react-native';
+import { ThemeProvider } from '@shopify/restyle';
+import Pressable from './Pressable';
+import Text from '../Text';
+import themeMaestro from '../../themes/maestro';
+
+afterEach(cleanup);
+
+jest.mock('react-native-vector-icons/MaterialCommunityIcons', () => 'Icon');
+
+test('should execute press event function', () => {
+  const onEventMock = jest.fn();
+  const { getByTestId } = render(
+    <ThemeProvider theme={themeMaestro}>
+      <Pressable onPress={onEventMock}>
+        <Text>Pressable</Text>
+      </Pressable>
+    </ThemeProvider>,
+  );
+
+  fireEvent.press(getByTestId('pressable'));
+  expect(onEventMock).toHaveBeenCalledTimes(1);
+});
+
+test('should be black', () => {
+  const onEventMock = jest.fn();
+  const { getByTestId } = render(
+    <ThemeProvider theme={themeMaestro}>
+      <Pressable onPress={onEventMock} boxProps={{ bg: 'black' }}>
+        <Text>Pressable</Text>
+      </Pressable>
+    </ThemeProvider>,
+  );
+
+  expect(getByTestId('pressable-box').props.style[0].backgroundColor).toBe(
+    '#000000',
+  );
+});
+
+test('should render correctly', async () => {
+  const { toJSON } = render(
+    <ThemeProvider theme={themeMaestro}>
+      <Pressable onPress={() => undefined}>
+        <Text>Label Pressable</Text>
+      </Pressable>
+    </ThemeProvider>,
+  );
+
+  expect(toJSON()).toMatchSnapshot();
+});
---
+++ src/components/Pressable/interfaces.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+import { PressableProps } from 'react-native';
+import { CustomBoxProps } from '../Box/interface';
+
+export type CustomPressableProps = PressableProps &
+  Partial<{
+    children: React.ReactNode;
+    boxProps: CustomBoxProps;
+  }>;
