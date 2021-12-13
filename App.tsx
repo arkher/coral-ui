fd8bf025c898e62ed032b811442683b9b2622773
Paulo Gomes <paulo.rgomes22@gmail.com>
2021-12-13 19:10:08 -0300

    fix(reanimated): adicionando gesture handler e plugin do babel
    ajustando problema que acontecia ao utilizar o reanimated

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable camelcase */
+import 'react-native-gesture-handler';
 import React from 'react';
 import { Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
