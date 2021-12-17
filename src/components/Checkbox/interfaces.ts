5c8e060a40876ebe202beb0458312cce17ccc740
Paulo Gomes <paulo.rgomes22@gmail.com>
2021-12-17 10:54:00 -0300

    feat(release): removendo componentes para lançamento da release

--- src/components/Checkbox/interfaces.ts
+++
@@ -1,7 +0,0 @@
-export type CheckboxProps = {
-  label?: string;
-  value: boolean;
-  onChange: () => void;
-  disabled?: boolean;
-  required?: boolean;
-};
--- src/components/Switch/interfaces.ts
+++
@@ -1,5 +0,0 @@
-export type SwitchProps = {
-  onChange(value: boolean): void;
-  value: boolean;
-  disabled?: boolean;
-};
