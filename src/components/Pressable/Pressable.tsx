ff6ac9973fc28a8b58e811761acd84f71de15f02
Paulo Gomes <paulo.rgomes22@gmail.com>
2021-12-17 20:22:21 -0300

    fix(pressable): corrigindo problemas de espaçamento
    corrigindo margin e padding; separando props do pressable da box; ajuste no token hilight-03-base

--- src/components/Pressable/Pressable.tsx
+++ src/components/Pressable/Pressable.tsx
@@ -1,12 +1,25 @@
 /* eslint-disable camelcase */
+import { createRestyleComponent, useTheme } from '@shopify/restyle';
 import React from 'react';
 import { Pressable, ViewStyle } from 'react-native';
+import {
+  borderRadius,
+  borderWidth,
+  heightComponent,
+  opacity,
+  shadowOffset,
+  shadowOpacity,
+  shadowRadius,
+  Theme,
+} from '../../themes';
 import Box from '../Box';
 import { CustomPressableProps } from './interfaces';
-import useSyles from './Pressable.styles';
+import useStyles from './Pressable.styles';
 
 const CustomPressable: React.FC<CustomPressableProps> = ({
   children,
+  style,
+  testID,
   onPress,
   onPressIn,
   onPressOut,
@@ -20,12 +33,15 @@ const CustomPressable: React.FC<CustomPressableProps> = ({
   android_disableSound,
   android_ripple,
   testOnly_pressed,
-  style,
-  testID,
+  p,
+  pb,
+  pl,
+  pr,
+  pt,
   ...props
 }: CustomPressableProps) => {
-  const styles = useSyles();
-
+  const styles = useStyles();
+  const { spacing } = useTheme<Theme>();
   const pressableProps = {
     onPress,
     onPressIn,
@@ -43,22 +59,52 @@ const CustomPressable: React.FC<CustomPressableProps> = ({
   };
 
   return (
-    <Pressable
-      testID={testID || 'pressable'}
-      style={({ pressed }) => [
-        {
-          elevation: pressed ? 1 : 3,
-        },
-        { ...(style as ViewStyle) },
-        { ...styles.customStyle },
-      ]}
-      {...pressableProps}
+    <Box
+      bg="white"
+      minHeight="auto"
+      minWidth="auto"
+      br="md"
+      width="100%"
+      testID="pressable-box"
+      {...props}
     >
-      <Box ml="xs" mr="xs" mb="nano" testID="pressable-box" {...props}>
+      <Pressable
+        testID={testID || 'pressable'}
+        style={({ pressed }) => [
+          {
+            elevation: pressed ? 1 : 3,
+          },
+          {
+            padding: spacing[p as 'quark' | 'nano' | 'xs' | 'sm' | 'md' | 'lg'],
+            paddingTop:
+              spacing[pt as 'quark' | 'nano' | 'xs' | 'sm' | 'md' | 'lg'],
+            paddingBottom:
+              spacing[pb as 'quark' | 'nano' | 'xs' | 'sm' | 'md' | 'lg'],
+            paddingRight:
+              spacing[pr as 'quark' | 'nano' | 'xs' | 'sm' | 'md' | 'lg'],
+            paddingLeft:
+              spacing[pl as 'quark' | 'nano' | 'xs' | 'sm' | 'md' | 'lg'],
+          },
+          { ...(styles.customStyle as ViewStyle) },
+          { ...(style as ViewStyle) },
+        ]}
+        {...pressableProps}
+      >
         {children}
-      </Box>
-    </Pressable>
+      </Pressable>
+    </Box>
   );
 };
 
-export default CustomPressable;
+export default createRestyleComponent<CustomPressableProps, Theme>(
+  [
+    borderRadius,
+    borderWidth,
+    opacity,
+    shadowOffset,
+    shadowRadius,
+    shadowOpacity,
+    heightComponent,
+  ],
+  CustomPressable,
+);
